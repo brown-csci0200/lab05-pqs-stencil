5aebf965b1abfe1b1e5eac4ff05f7eb27df102dc
MLi0411 <92190319+MLi0411@users.noreply.github.com>
2025-10-05 13:04:02 -0400

    fix task order

--- sol/MainParenthesis.java
+++ sol/MainParenthesis.java
@@ -16,7 +16,7 @@ public class MainParenthesis {
 
     ParenthesisChecker checker = new ParenthesisChecker();
     
-    // TODO: Task 9, uncomment the try-catch block below and then fill it out!
+    // TODO: Task 8, uncomment the try-catch block below and then fill it out!
     // try {
      
     // } catch (ParenthesisExceptionStencil e) {
